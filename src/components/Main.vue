ad518b636b6348b6d9094d13e9d26ac1d06c8d27
wachcio <wachcio@poczta.fm>
2020-04-28 23:26:55 +0200

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="wrapper"><h1>Książka kucharska</h1></div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props:  {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  components:  {
+
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  created() {
+
+  },
+  watch: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
