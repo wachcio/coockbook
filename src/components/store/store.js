d093b78fca99f3accd11c4eb0314eb47a57e6749
wachcio <wachcio@poczta.fm>
2020-05-04 02:11:14 +0200

    change base URL

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -3,8 +3,8 @@ import Vuex from 'vuex';
 import axios from 'axios';
 // import _ from 'lodash';
 
-// axios.defaults.baseURL = 'https://cookbookapi.wachcio.pl/';
-axios.defaults.baseURL = 'http://localhost:8000/';
+axios.defaults.baseURL = 'https://cookbookapi.wachcio.pl/';
+// axios.defaults.baseURL = 'http://localhost:8000/';
 axios.defaults.headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
