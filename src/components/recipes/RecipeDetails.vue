09f0d8cc157a0b6c8328cfa8cbc3bffc3d36a2eb
wachcio <wachcio@poczta.fm>
2020-05-13 21:58:15 +0200

    Add basic details css

--- src/components/recipes/RecipeDetails.vue
+++ src/components/recipes/RecipeDetails.vue
@@ -1,11 +1,27 @@
 <template>
-    <div v-if="recipe">
-        <H1>{{ recipe.name }}</H1>
+    <div class="recipe_details">
+        <div v-if="recipe">
+            <H1 class="recipe_details__name">{{ recipe.name }}</H1>
+            <p class="recipe_details__description">
+                Opis: {{ recipe.description }}
+            </p>
+            <p class="recipe_details__ingredients">
+                Składniki: {{ recipe.ingredients }}
+            </p>
+            <p class="recipe_details__execution">
+                Wykonanie: {{ recipe.execution }}
+            </p>
+            <StarRating
+                class="recipe_details__rating"
+                :rating="recipe.rating"
+            />
+        </div>
     </div>
 </template>
 
 <script>
 import { mapState, mapMutations, mapActions, mapGetters } from 'vuex';
+import StarRating from './StarRecipeCard';
 let slug = require('slug');
 import _ from 'lodash';
 export default {
@@ -16,7 +32,7 @@ export default {
     data() {
         return {};
     },
-    components: {},
+    components: { StarRating },
     methods: {
         ...mapMutations([
             'updateRecipes',
@@ -66,4 +82,27 @@ export default {
 
 <style lang="scss" scoped>
 @import './../../style/main.scss';
+
+.recipe_details {
+    width: 95%;
+    min-height: 85vh;
+
+    padding: 15px;
+
+    border-radius: 21px;
+    background: #ffd024;
+    box-shadow: inset 7px 7px 15px #ebbf21, inset -7px -7px 15px #ffe127;
+
+    &__name {
+        text-align: center;
+
+        &::first-letter {
+            text-transform: uppercase;
+        }
+    }
+    &__rating {
+        bottom: auto;
+        padding-top: 2em;
+    }
+}
 </style>
